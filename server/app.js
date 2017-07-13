bfb7614e6d90ef118b2eacc934e44e0f37587d22
tolikrss <tolikrss@gmail.com>
2017-07-13 19:26:40 +0300

    upload file bug fixed

--- server/app.js
+++ server/app.js
@@ -58,91 +58,41 @@ app.post('/upload', upload.single('file'), (req, res) => {
     fs.readFile(`uploads/${file.originalname}`, function(err, data) {
         if (err) throw err;
 
-        var arrayStr = data.toString().split("\n");
+        var arrayStr = data.toString().split("\n\n");
         var arrayObj = [];
 
-        console.log("arrayStr.length - " + arrayStr.length);
-        console.log("arrayStr[ arrayStr.length -1 ] : ");
-        console.log(arrayStr[arrayStr.length - 1]);
-        console.log("arrayStr[ arrayStr.length -2 ] : ");
-        console.log(arrayStr[arrayStr.length - 2]);
-        console.log("arrayStr: ");
-        console.log(arrayStr);
-
-        var j = 0;
-        var elemArrayObj = {};
-        var arrayStars = [];
         for (var i = 0; i < arrayStr.length; i++) {
-            if (arrayStr[i] !== '') {
-                if (j = 0) {
-                    elemArrayObj.title = arrayStr[i].substring(7);
-                    j++;
-                    continue;
-                }
-                if (j = 1) {
-                    elemArrayObj.releaseYear = arrayStr[i].substring(14);
-                    j++;
-                    continue;
-                }
-                if (j = 2) {
-                    elemArrayObj.format = arrayStr[i].substring(8);
-                    j++;
-                    continue;
-                }
-                if (j = 3) {
-                    arrayStars = arrayStr[i][3].substring(7).split(', ');
-                    elemArrayObj.stars = arrayStars;
-                    arrayStars = [];
-                    j++;
-                    continue;
-                }
+            arrayStr[i] = arrayStr[i].split("\n");
+        }
+        var i = 0;
+        while (i < arrayStr.length) {
+            while (arrayStr[i].indexOf("") !== -1) {
+                arrayStr[i].splice(arrayStr[i].indexOf(""), 1);
+            }
+            if (arrayStr[i].length === 0) {
+                arrayStr.splice(i, 1);
             } else {
-                arrayObj.push(elemArrayObj);
-                elemArrayObj = {};
-                j = 0;
+                i++;
             }
         }
-        // arrayStr.forEach(function(elem, i, array) {
+        arrayStr.forEach(function(elem, i, array) {
+            let elemArrayObj = {};
+            let arrayStars = [];
+            elemArrayObj.title = elem[0].substring(7);
+            elemArrayObj.releaseYear = elem[1].substring(14);
+            elemArrayObj.format = elem[2].substring(8);
+            arrayStars = elem[3].substring(7).split(', ');
+            elemArrayObj.stars = arrayStars;
+            arrayObj.push(elemArrayObj);
+        });
 
-        //     if (elem !== '') {
-        //         if (j = 0) {
-        //             elemArrayObj.title = elem.substring(7);
-        //             j++;
-        //             return;
-        //         }
-        //         if (j = 1) {
-        //             elemArrayObj.releaseYear = elem.substring(14);
-        //             j++;
-        //             return;
-        //         }
-        //         if (j = 2) {
-        //             elemArrayObj.format = elem.substring(8);
-        //             j++;
-        //             return;
-        //         }
-        //         if (j = 3) {
-        //             arrayStars = elem[3].substring(7).split(', ');
-        //             elemArrayObj.stars = arrayStars;
-        //             arrayStars = [];
-        //             j++;
-        //             return;
-        //         }
-        //     } else {
-        //         arrayObj.push(elemArrayObj);
-        //         elemArrayObj = {};
-        //         j = 0;
-        //     }
-        // });
-
-        console.log("arrayObj: ");
-        console.log(arrayObj);
-
-
-        // db.uploadFilms(arrayObj).then((data) => res.send(data));
+        db.uploadFilms(arrayObj).then((data) => res.send(data));
     });
 });
 
-//work's on not correct file
+// //===========================================================
+// // work's on file with not correct structure
+// //=============================================================
 // app.post('/upload', upload.single('file'), (req, res) => {
 //     const file = req.file; // file passed from client
 //     const meta = req.body; // all other values passed from the client, like name, etc..
@@ -180,6 +130,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
 //         db.uploadFilms(arrayObj).then((data) => res.send(data));
 //     });
 // });
+// //==========================================================
 
 app.delete('/films/:id', (req, res) => {
     db.deleteFilm(req.params.id).then((data) => res.send(data));
